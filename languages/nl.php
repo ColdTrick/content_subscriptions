016faa02fd70ca3ac3087d6e779f5ebee41e3fe5
Jerome Bakker <jeabakker@coldtrick.com>
2014-03-27 09:47:24 +0100

    added: Dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,18 @@
+<?php
+$language = array (
+  'content_subscriptions:subscribe' => 'Volgen',
+  'content_subscriptions:unsubscribe' => 'Ontvolgen',
+  'content_subscriptions:sidebar:title' => 'Blijf op de hoogte',
+  'content_subscriptions:sidebar:owner' => 'Je bent de eigenaar van deze content en blijft altijd op de hoogte.',
+  'content_subscriptions:sidebar:notifications' => 'Je blijft al op de hoogte door je groep notificatie instellingen.',
+  'content_subscriptions:sidebar:counter' => '%s leden volgen dit.',
+  'content_subscriptions:sidebar:no_subscriptions' => 'Niemand volgt dit.',
+  'content_subscriptions:subscribe:description' => 'Als je op de hoogte wilt blijven van deze content, klik Volgen',
+  'content_subscriptions:unsubscribe:description' => 'Als je niet langer op de hoogte wilt blijven van deze content, klik Ontvolgen.',
+  'content_subscriptions:action:subscribe:error:owner' => 'Je bent de eigenaar van deze content en kan niet (ont)volgen',
+  'content_subscriptions:action:subscribe:error:subscribe' => 'Er is een onbekende fout opgetreden tijdens het volgen, probeer het nogmaals',
+  'content_subscriptions:action:subscribe:error:unsubscribe' => 'Er is een onbekende fout opgetreden tijdens het ontvolgen, probeer het nogmaals',
+  'content_subscriptions:action:subscribe:success:subscribe' => 'Je blijft nu op de hoogte van deze content',
+  'content_subscriptions:action:subscribe:success:unsubscribe' => 'Je blijft niet langer op de hoogte van deze content',
+);
+add_translation("nl", $language);
