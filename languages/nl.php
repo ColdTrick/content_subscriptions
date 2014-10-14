d43fddc5a2c9b9b1bdce6a1281ede425eb3fe68b
Jerome Bakker <jeabakker@coldtrick.com>
2014-10-14 16:19:36 +0200

    updated Dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -1,30 +1,27 @@
 <?php
 /**
  * This file was created by Translation Editor v2.0
- * On 2014-07-17 09:18
+ * On 2014-10-14 16:19
  */
 
-$language = array (
-  'content_subscriptions:generic_comment:subject' => 'Nieuwe reactie op: %s',
-  'content_subscriptions:generic_comment:body' => '%s reageerde op %s:
+return array (
+  'admin:upgrades:content_subscriptions' => 'Content volgen',
+  'content_subscriptions:settings:description' => 'Ontvang notificaties als er wordt gereageerd op content die je volgt',
+  'content_subscriptions:create:comment:subject' => 'Nieuwe reactie op: %s',
+  'content_subscriptions:create:comment:summary' => 'Nieuwe reactie op: %s',
+  'content_subscriptions:create:comment:message' => 'Hallo %s,
+
+%s reageerde op %s:
 
 %s
 
-Bekijk en reageer hier:
+Bekijk en reageer op de content:
 %s',
   'content_subscriptions:subscribe' => 'Volgen',
   'content_subscriptions:unsubscribe' => 'Ontvolgen',
-  'content_subscriptions:sidebar:title' => 'Blijf op de hoogte',
-  'content_subscriptions:sidebar:owner' => 'Je bent de eigenaar van deze content en blijft altijd op de hoogte.',
-  'content_subscriptions:sidebar:notifications' => 'Je blijft al op de hoogte door je groep notificatie instellingen.',
-  'content_subscriptions:sidebar:counter' => '%s leden volgen dit.',
-  'content_subscriptions:sidebar:no_subscriptions' => 'Niemand volgt dit.',
-  'content_subscriptions:subscribe:description' => 'Als je op de hoogte wilt blijven van deze content, klik Volgen',
-  'content_subscriptions:unsubscribe:description' => 'Als je niet langer op de hoogte wilt blijven van deze content, klik Ontvolgen.',
   'content_subscriptions:action:subscribe:error:owner' => 'Je bent de eigenaar van deze content en kan niet (ont)volgen',
   'content_subscriptions:action:subscribe:error:subscribe' => 'Er is een onbekende fout opgetreden tijdens het volgen, probeer het nogmaals',
   'content_subscriptions:action:subscribe:error:unsubscribe' => 'Er is een onbekende fout opgetreden tijdens het ontvolgen, probeer het nogmaals',
   'content_subscriptions:action:subscribe:success:subscribe' => 'Je blijft nu op de hoogte van deze content',
   'content_subscriptions:action:subscribe:success:unsubscribe' => 'Je blijft niet langer op de hoogte van deze content',
 );
-add_translation("nl", $language);
