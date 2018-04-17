33dfe0ac79e06c96933fa6cc5ed7bdd79145517c
Jerome Bakker <jeabakker@coldtrick.com>
2018-04-17 09:53:20 +0200

    chore: code cleanup

--- actions/content_subscriptions/subscribe.php
+++ actions/content_subscriptions/subscribe.php
@@ -3,43 +3,38 @@
  * Toggle the subscription for a user
  */
 
-$user_guid = (int) get_input("user_guid", elgg_get_logged_in_user_guid());
-$entity_guid = (int) get_input("entity_guid");
+$user_guid = (int) get_input('user_guid', elgg_get_logged_in_user_guid());
+$entity_guid = (int) get_input('entity_guid');
 
 if (empty($user_guid) || empty($entity_guid)) {
-	register_error(elgg_echo("InvalidParameterException:MissingParameter"));
-	forward(REFERER);
+	return elgg_error_response(elgg_echo('error:missing_data'));
 }
 
 $user = get_user($user_guid);
 $entity = get_entity($entity_guid);
 	
 if (empty($user) || empty($entity)) {
-	register_error(elgg_echo("InvalidParameterException:NoEntityFound"));
-	forward(REFERER);
+	return elgg_error_response(elgg_echo('error:missing_data'));
 }
 
-// user cant be the owner
-if ($entity->getOwnerGUID() == $user->getGUID()) {
-	register_error(elgg_echo("content_subscriptions:action:subscribe:error:owner"));
-	forward(REFERER);
+// user can't be the owner
+if ($entity->owner_guid === $user->guid) {
+	return elgg_error_response(elgg_echo('content_subscriptions:action:subscribe:error:owner'));
 }
 
 // subscribe or unsubscribe
-if (content_subscriptions_check_subscription($entity->getGUID(), $user->getGUID())) {
+if (content_subscriptions_check_subscription($entity->guid, $user->guid)) {
 	// unsubscribe
-	if (content_subscriptions_unsubscribe($entity->getGUID(), $user->getGUID())) {
-		system_message(elgg_echo("content_subscriptions:action:subscribe:success:unsubscribe"));
-	} else {
-		register_error(elgg_echo("content_subscriptions:action:subscribe:error:unsubscribe"));
+	if (content_subscriptions_unsubscribe($entity->guid, $user->guid)) {
+		return elgg_ok_response('', elgg_echo('content_subscriptions:action:subscribe:success:unsubscribe'));
 	}
-} else {
-	// subscribe
-	if (content_subscriptions_subscribe($entity->getGUID(), $user->getGUID())) {
-		system_message(elgg_echo("content_subscriptions:action:subscribe:success:subscribe"));
-	} else {
-		register_error(elgg_echo("content_subscriptions:action:subscribe:error:subscribe"));
-	}
+	
+	return elgg_error_response(elgg_echo('content_subscriptions:action:subscribe:error:unsubscribe'));
 }
 
-forward(REFERER);
+// subscribe
+if (content_subscriptions_subscribe($entity->guid, $user->guid)) {
+	return elgg_ok_response('', elgg_echo('content_subscriptions:action:subscribe:success:subscribe'));
+}
+
+return elgg_error_response(elgg_echo('content_subscriptions:action:subscribe:error:subscribe'));
