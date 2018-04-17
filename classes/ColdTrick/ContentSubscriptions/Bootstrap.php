2a315eaff0e8441ee512b5b2529c78dc07143b26
Jerome Bakker <jeabakker@coldtrick.com>
2018-04-17 14:48:16 +0200

    changed: moved start.php content to Bootstrap class

---
+++ classes/ColdTrick/ContentSubscriptions/Bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ColdTrick\ContentSubscriptions;
+
+use Elgg\DefaultPluginBootstrap;
+
+class Bootstrap extends DefaultPluginBootstrap {
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public function init() {
+		
+		// settings
+		elgg_extend_view('notifications/settings/other', 'content_subscriptions/notifications/settings');
+		
+		// register event handlers
+		$events = $this->elgg()->events;
+		$events->registerHandler('create', 'object', __NAMESPACE__ . '\Comments::createObject');
+		$events->registerHandler('create', 'annotation', __NAMESPACE__ . '\Likes::create');
+		
+		// register plugin hooks
+		$hooks = $this->elgg()->hooks;
+		$hooks->registerHandler('register', 'menu:entity', __NAMESPACE__ . '\EntityMenu::register');
+		$hooks->registerHandler('get', 'subscriptions', __NAMESPACE__ . '\Subscriptions::verifySubscribersSettings', 400);
+		$hooks->registerHandler('get', 'subscriptions', __NAMESPACE__ . '\Subscriptions::removeUnsubscribedGroupMembers', 999);
+		$hooks->registerHandler('response', 'action:notifications/settings', __NAMESPACE__ . '\UserSettings::notificationSettingsSaveAction');
+	}
+}
--- elgg-plugin.php
+++ elgg-plugin.php
@@ -1,6 +1,13 @@
 <?php
 
+define('CONTENT_SUBSCRIPTIONS_BLOCK', 'content_block_subscription');
+
+require_once(dirname(__FILE__) . '/lib/functions.php');
+
+use ColdTrick\ContentSubscriptions\Bootstrap;
+
 return [
+	'bootstrap' => Bootstrap::class,
 	'actions' => [
 		'content_subscriptions/subscribe' => [],
 	],
