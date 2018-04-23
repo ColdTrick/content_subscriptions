e0d2b758f977beed1b834d9621350c846e63b01b
Dennis Ploeger <github.dennis@dieploegers.de>
2018-04-23 10:09:27 +0200

    added: German translation

---
+++ languages/de.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * English translation for this plugin
+ */
+
+return [
+    'content_subscriptions:subscribe' => "Abonnieren",
+    'content_subscriptions:unsubscribe' => "Abbestellen",
+
+    'content_subscriptions:settings:description' => "Erhalte Benachrichtigungen, wenn Kommentare auf Deine abonnierten Inhalte gepostet werden",
+
+    // settings
+    'content_subscriptions:settings:likes' => "Bei einem Like automatisch abonnieren?",
+    'content_subscriptions:settings:likes:description' => "Wenn Du etwas mit einem Like versiehst, abonnierst Du den Inhalt automatisch. (Nur verfügbar, wenn das Like-Plugin aktiviert ist)",
+
+    // actions
+    'content_subscriptions:action:subscribe:error:owner' => "Du bist der Besitzer dieses Inhalts und kannst ihn weder abonnieren noch abbestellen",
+    'content_subscriptions:action:subscribe:error:subscribe' => "Ein unbekannter Fehler ist beim abonnieren aufgetreten. Bitte versuche es nochmal",
+    'content_subscriptions:action:subscribe:error:unsubscribe' => "Ein unbekannter Fhler ist beim abbestellen aufgetreten . Bitte versuche es nochmal",
+    'content_subscriptions:action:subscribe:success:subscribe' => "Du hast diesen Inhalt erfolgreich abonniert",
+    'content_subscriptions:action:subscribe:success:unsubscribe' => "Das Abonnement wurde erfolgreich abbestellt",
+];
