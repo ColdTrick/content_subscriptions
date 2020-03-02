1fb0f0e662a2413fa41075840981a431fbe37c8f
Jeroen Dalsem <jdalsem@coldtrick.com>
2020-03-02 09:24:42 +0100

    added: date selection for advanced statistics reports

--- views/default/admin/advanced_statistics/content_subscriptions.php
+++ views/default/admin/advanced_statistics/content_subscriptions.php
@@ -1,11 +1,14 @@
 <?php
 
+echo elgg_view('advanced_statistics/date_selector');
+
 echo elgg_view('advanced_statistics/elements/chart', [
 	'title' => elgg_echo('advanced_statistics:content_subscriptions:subscribers'),
 	'id' => 'advanced-statistics-content-subscriptions-subscribers',
 	'page' => 'admin_data',
 	'section' => 'content_subscriptions',
 	'chart' => 'subscribers',
+	'date_limited' => true,
 ]);
 
 echo elgg_view('advanced_statistics/elements/chart', [
@@ -14,4 +17,5 @@ echo elgg_view('advanced_statistics/elements/chart', [
 	'page' => 'admin_data',
 	'section' => 'content_subscriptions',
 	'chart' => 'blockers',
+	'date_limited' => true,
 ]);
