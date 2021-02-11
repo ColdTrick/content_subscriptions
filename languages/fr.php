8bcad37ab9413f5f559bb6eac85cfeafcb52605d
Jerôme Bakker <jeabakker@coldtrick.com>
2021-02-11 09:00:19 +0100

    added: French translation
    for v 6.1
    
    Co-authored-by: Dan d'Auge <76149200+danhype@users.noreply.github.com>

---
+++ languages/fr.php
@@ -0,0 +1,17 @@
+<?php
+
+return array (
+  'content_subscriptions:subscribe' => 'S\'abonner',
+  'content_subscriptions:unsubscribe' => 'Se désabonner',
+  'admin:advanced_statistics:content_subscriptions' => 'Abonnements de contenu',
+  'advanced_statistics:content_subscriptions:subscribers' => 'Abonnés',
+  'advanced_statistics:content_subscriptions:blockers' => 'Bloqueurs',
+  'content_subscriptions:settings:description' => 'Recevoir des notifications lorsque des commentaires sont publiés sur le contenu auquel vous vous êtes abonné',
+  'content_subscriptions:settings:likes' => 'Abonnement automatique au contenu sur les "J\'aime"',
+  'content_subscriptions:settings:likes:description' => 'Lorsque vous aimez un contenu, vous vous abonnez également aux mises à jour, de la même manière que lorsque vous laissez un commentaire. Cela ne s\'applique que lorsque le plugin Like est activé.',
+  'content_subscriptions:action:subscribe:error:owner' => 'Vous êtes le créateur du contenu et ne pouvez pas vous (dé)abonner aux mises à jour',
+  'content_subscriptions:action:subscribe:error:subscribe' => 'Une erreur inconnue s\'est produite lors de I\'abonnement, veuillez réessayer',
+  'content_subscriptions:action:subscribe:error:unsubscribe' => 'Une erreur inconnue s\'est produite lors du désabonnement, veuillez réessayer',
+  'content_subscriptions:action:subscribe:success:subscribe' => 'Vous avez été abonné avec succès aux mises à jour de cet élément',
+  'content_subscriptions:action:subscribe:success:unsubscribe' => 'Vous avez été désabonné avec succès des mises à jour de cet élément',
+);
